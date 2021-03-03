93a30acdb85a370bfdb30d82ad7ed9bf341982e4
vihantyagi <47202142+vihantyagi@users.noreply.github.com>
2021-03-03 15:05:38 +0530

    Create OpenAndRead_Tiff.py

---
+++ Read_Tiff/OpenAndRead_Tiff.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  1 16:56:08 2021
+
+@author: dp
+"""
+
+from osgeo import gdal
+
+def getGeoTiffasArray(path_to_tiff):
+    dataset = gdal.Open(path_to_tiff)
+    image = dataset.ReadAsArray()
+    return image
+
+def openWithGdal(path_to_tiff):
+    data_set = gdal.Open(path_to_tiff)
+    return data_set
+
+def readBands(path_to_tiff):
+    # '/huser/users/trainee/vihan/Ahmedabad/AHM_BAND/16334311-518-1-5-X-STUC00OTD-_hrdpd_Sarkhej_India_'
+    files = [1,2,3]
+    mylist = []
+    for file in files:
+        f = folder + '/BAND%d'%file + '.tif'
+        arr = getGeoTiffasArray(f)
+        mylist.append((arr))
+    return mylist
+
