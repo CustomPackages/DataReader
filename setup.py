52607a5494f6d5c189584b7219a114044bc102c7
paulSubha998 <79894056+paulSubha998@users.noreply.github.com>
2021-03-03 10:30:36 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as readme_file:
 requirements = ["ipython>=6", "numpy", "gdal"]
 
 setup(
-    name="DataReader",
+    name="paulSubha998",
     version="0.0.1",
     author="Subhajit Paul",
     author_email="paul.isro123@gmail.com",
     description="A package to read tif image",
     long_description=readme,
     long_description_content_type="text/markdown",
-    url="",
+    url="https://github.com/CustomPackages/DataReader",
     packages=find_packages(),
     install_requires=requirements,
     classifiers=[
