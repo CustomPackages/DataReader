5419ebb12a0f6748918110428ea35abce16daf19
paulSubha998 <79894056+paulSubha998@users.noreply.github.com>
2021-03-03 17:59:58 +0530

    Update __init__.py

--- ReadData/__init__.py
+++ ReadData/__init__.py
@@ -1,5 +1,5 @@
 from .readTif import ReadTif
-from .LtoP import LATLONG_TO_XY
-from .PtoL import XY_TO_LATLONG
+from .LatLong_toPix import LATLONG_TO_XY
+from .Pix_toLatLong import XY_TO_LATLONG
 from .RGB_Crop import saveCrop
 from .read import read
