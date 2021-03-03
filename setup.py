43621dfba84c31f5866b6995c05c081d09102ce4
paulSubha998 <79894056+paulSubha998@users.noreply.github.com>
2021-03-03 11:47:17 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/CustomPackages/DataReader",
-    packages=['ReadData'],
+    packages=find_packages(),
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3.7",
