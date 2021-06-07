8c8ddc1354ffee69d53889e9c6d487461008bbf4
paulSubha998 <79894056+paulSubha998@users.noreply.github.com>
2021-06-07 10:55:34 +0530

    Update __init__.py

--- ReadData/__init__.py
+++ ReadData/__init__.py
@@ -2,6 +2,6 @@ from .readTif import ReadTif
 from .LatLong_toPix import LATLONG_TO_XY
 from .Pix_toLatLong import XY_TO_LATLONG
 from .RGB_Crop import saveCrop
-from .read import read
-from .get_tiles import get_tiles
+from .readImageData import readImageData
+from .get_tiles import get_tiles, save_npy, save_png
 from .NormData import normalize_image, find_mean, find_stdev, Gaussian_normalize
